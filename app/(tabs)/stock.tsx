3f09ff6d38e3eb5badd329a84f1bb7f909ae832b
Robin Fantino <dev@scanandstock.fr>
2024-11-27 11:48:55 +0100

    change stock for static presentation

--- app/(tabs)/stock.tsx
+++ app/(tabs)/stock.tsx
@@ -1,23 +1,55 @@
-import { View, Text, SafeAreaView, Image, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import React from 'react';
 import icons from '@/constant/icons';
 import TitleCustome from '@/components/TitleCustome';
+import { dataProduct } from '@/assets/dataTest';
+import fournisseurs from './fournisseurs';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 const Stock = () => {
+
+  
   return (
     <View style={styles.container}>
       <TitleCustome title="STOCK" />
 
-      <View style={styles.body}>
-        <Image source={icons.maintenance} />
-        <Text style={styles.textBis}>
-          La page <Text style={styles.textBold}>"Stock"</Text> est en cours de développement.
-        </Text>
-        <Text style={styles.textBis}>Nous vous avertirons lorsqu'elle sera disponible.</Text>
-        <Text style={styles.textBis}>
-          En attendant, vous pouvez <Text style={styles.textBold}>accéder à cet onglet via votre navigateur sur ordinateur ou tablette.</Text>
-        </Text>
+      <View style={[styles.row, styles.option]}>
+        <TouchableOpacity>MON STOCK</TouchableOpacity>
+        <TouchableOpacity>ALERTES</TouchableOpacity>
+        <TouchableOpacity>PEREMPTIONS</TouchableOpacity>
       </View>
+      <View style={[styles.row, {marginBottom: 5}]}>
+        <Text style={{ fontFamily: 'Montserrat-Regular'}}>DESIGNATION</Text>
+        <Text>ALERTES</Text>
+        <Text>STOCK</Text>
+      </View>
+      <FlatList
+        data={dataProduct}
+        style={styles.productsContainer}
+        renderItem={({ item }) => (
+          <View style={styles.product}>
+            {item.stock < item.alerte ? (
+              <LinearGradient colors={['#FFFFFF', '#F9C6C6']} end={{ x: 0.5, y: 0 }} style={styles.background} />
+            ) : 
+            <LinearGradient colors={['#FFFFFF', '#B8ECE9']} end={{ x: 0.5, y: 0 }}  style={styles.background} />
+            }
+            <View style={styles.designation}>
+              <Text style={styles.name} >{item.designation}</Text>
+              <Text style={styles.fournisseur}> <Image style={{width:15, height:15}} source={icons.darkTruck} /> {item.fournisseur} </Text>
+              <Text style={styles.category}><Image style={{width:15, height:15}} source={icons.categorie} /> {item.categorie}</Text>
+            </View>
+            <View>
+              <Text>{item.alerte}</Text>
+            </View>
+            <View style={[styles.row, styles.stock]}>
+              <TouchableOpacity style={[styles.stc, styles.right]}>-</TouchableOpacity>
+              <View  style={styles.stc}><Text>{item.stock}</Text></View>
+              <TouchableOpacity style={[styles.stc, styles.left]}>+</TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -25,6 +57,41 @@ const Stock = () => {
 export default Stock;
 
 const styles = StyleSheet.create({
+  background: {
+    height: '100%',
+    borderRadius: 10,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    zIndex: -1,
+  },
+  right: {
+    borderRightWidth: 2,
+    borderRightColor: '#84949C',
+  },
+  left: {
+    borderLeftWidth: 2,
+    borderLeftColor: '#84949C',
+  },
+  stc: {
+    height: '100%', 
+    width: '33%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 37
+  },
+  stock: {
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#84949C',
+    width: 100,
+    height: 40,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
   container: {
     flex: 1,
     padding: 10,
@@ -44,9 +111,43 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   textBold: {
-    fontFamily: 'Montserrat-Regular',
+    fontFamily: 'Montserrat-Bold',
     fontSize: 16,
-    fontWeight: '700',
+  },
+  option: {
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  productsContainer: {
+    flex: 1,
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+  },
+  product: {
+    height: 120,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  designation: {
+    width: '50%',
+  },
+  name:{
+    fontFamily: 'Montserrat-Bold',
+  },
+  fournisseur: {
+    fontFamily: 'Montserrat-Thin',
+    fontSize: 12,
+  },
+  category: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 12,
   },
 });
-
